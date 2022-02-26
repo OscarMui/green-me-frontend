0875cf024f89b1a0db1a6ee60025f95648acf5e1
Oscar Mui <oscarmui1127@gmail.com>
2022-02-26 15:12:14 +0000

    reformatted questionnaire post output

--- dist/js/questionnaire.js
+++ dist/js/questionnaire.js
@@ -1,9 +1,11 @@
-function changeYesNo(i){
-    let yesNo = $('input[name="yesNo'+i+'"]:checked').val();
+function changeYesNo(i,showSubquestion){
+    // let yesNo = $('input[name="yesNo'+i+'"]:checked').val();
     // console.log(i,yesNo)
-    if(yesNo == "no"){
+    if(showSubquestion){
         $("#subQuestion"+i).removeClass("d-none");
+        $("#radioLikeliness1"+i).prop('required', 'true');
     }else{
         $("#subQuestion"+i).addClass("d-none");
+        $("#radioLikeliness1"+i).prop('required', undefined);
     }
 }
--- routes/views/questionnaire.js
+++ routes/views/questionnaire.js
@@ -2,23 +2,43 @@ const {submitQuestionnaire} = require("../api/submitQuestionnaire");
 
 exports = module.exports = function (req, res, next) {
     console.log(req.method);
-    console.log(req.body);
+    // console.log(req.body);
 
     let method = req.method;
+    let body = req.body;
     
 
     const questions = [
         {
             name: "Do you bring a bag to the supermarket?",
+            subquestionOption: "no",
         },
         {
             name: "Do you turn on lights in the day?",
+            subquestionOption: "no",
+        },
+        {
+            name: "Do you eat beef?",
+            subquestionOption: "yes",
         },
     ]
 
     if(method=="POST"){
         console.log(req.body);
-        let answers = req.body;
+        let answers = new Array();
+        let i = 0;
+        while(body["yesNo"+i]){
+            let answer = {
+                questionId: i,
+                answer1: body["yesNo"+i],
+            }
+            if(body["likeliness"+i]){
+                answer.answer2 = body["likeliness"+i]
+            }
+            answers.push(answer);
+            i++;
+        }
+        console.log(answers);
         submitQuestionnaire(answers).then((result)=>{
             res.render("questionnaire",{method,result});
         });
